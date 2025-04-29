f8497979327c53e9cf81fbf60178d48ba4462917
Deepak-123456789 <deepak8006360@gmail.com>
2025-04-29 13:04:43 +0530

    added

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -23,14 +23,20 @@ function Universe() {
             </p>
           </div>
           <div className="col-md-4 p-3">
-            <img src="media/Images/streak.png" alt="Streak logo" />
+            <img
+              src={`${process.env.PUBLIC_URL}/media/Images/streak.png`}
+              alt="Streak logo"
+            />
             <p className="text-small text-muted mt-2">
               Systematic trading platform that allows you to create and backtest
               strategies without coding.
             </p>
           </div>
           <div className="col-md-4 p-3">
-            <img src="media/Images/sensibull.png" alt="Sensibull logo" />
+            <img
+              src={`${process.env.PUBLIC_URL}/media/Images/sensibull.png`}
+              alt="Sensibull logo"
+            />
             <p className="text-small text-muted mt-4">
               Options trading platform that lets you create strategies, analyze
               positions, and examine data points like open interest, FII/DII,
@@ -38,21 +44,30 @@ function Universe() {
             </p>
           </div>
           <div className="col-md-4 p-3">
-            <img src="media/Images/tijori.png" alt="Tijori logo" />
+            <img
+              src={`${process.env.PUBLIC_URL}/media/Images/tijori.png`}
+              alt="Tijori logo"
+            />
             <p className="text-small text-muted mt-2">
               Investment research platform that offers detailed insights on
               stocks, sectors, supply chains, and more.
             </p>
           </div>
           <div className="col-md-4 p-3">
-            <img src="media/Images/ditto.png" alt="Ditto Insurance logo" />
+            <img
+              src={`${process.env.PUBLIC_URL}/media/Images/ditto.png`}
+              alt="Ditto Insurance logo"
+            />
             <p className="text-small text-muted mt-2">
               Personalized advice on life and health insurance. No spam and no
               mis-selling.
             </p>
           </div>
           <div className="col-md-4 p-3 mt-5 ">
-            <img src="media/Images/golden.png" alt="Smallcase logo" />
+            <img
+              src={`${process.env.PUBLIC_URL}/media/Images/golden.png`}
+              alt="Smallcase logo"
+            />
             <p className="text-small text-muted mt-4">
               Fixed-income investment platform offering curated bonds and
               debentures for steady returns.
